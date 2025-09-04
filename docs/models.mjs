0eef7939564ef0ed84051b02f34792bb27ca1f13
ThreeSteve <99689011+ThreeSteve@users.noreply.github.com>
2025-09-04 13:25:52 +0200

    Add files via upload
    Test Models

---
+++ docs/models.mjs
@@ -0,0 +1,27 @@
+export const models = [
+    {
+        "title": "Black (1976) Vector",
+        "path": "./black-penteract-vector.mjs"
+    },
+    {
+        "title": "Black-Scholes (1973)",
+        "path": "./black-scholes.mjs"
+    },
+    {
+        "title": "Matrix Product (Template)",
+        "path": "./matrix-product-template.mjs"
+    },
+    {
+        "title": "Matrix Product",
+        "path": "./matrix-product.mjs"
+    },
+    {
+        "title": "SABR Implied Volatility",
+        "path": "./sabr-implied-black-lognormal-volatility.mjs"
+    },
+    {
+        "title": "Svensson Interpolation",
+        "path": "./svennson-interpolate.mjs"
+    }
+];
+
